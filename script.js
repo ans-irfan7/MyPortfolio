e6239e89484c5f8bb023625fe7f08dccb7762982
Ans Irfan <ans.irfan07@gmail.com>
2026-03-12 01:25:18 +0500

    My portfolio website

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple interaction when contact button is clicked
+document.getElementById("contactBtn").addEventListener("click", function() {
+    alert("Thanks for visiting! You can email me at ans.irfan07@gmail.com");
+});
